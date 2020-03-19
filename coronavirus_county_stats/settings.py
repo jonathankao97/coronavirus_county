703c8fb8c871c16531660aec28000b1711743c48
Jonathan Kao <jkao97@yahoo.com>
2020-03-19 14:26:24 -0700

    update celery and heroku settings

--- coronavirus_county_stats/settings.py
+++ coronavirus_county_stats/settings.py
@@ -139,7 +139,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Celery application definition
-CELERY_BROKER_URL = 'redis://localhost:6379'
+BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
