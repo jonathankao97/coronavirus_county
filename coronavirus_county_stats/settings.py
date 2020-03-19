ee7fea86d63df960d80b1304c8e6e2b5f478876f
Jonathan Kao <jkao97@yahoo.com>
2020-03-18 18:09:17 -0700

    update settings

--- coronavirus_county_stats/settings.py
+++ coronavirus_county_stats/settings.py
@@ -22,8 +22,8 @@ TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "_i_rc8aii3-yhmw8@3wh8x&*d2oen%6123dlmk-po=+y*zd8tj^"
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG')
 DEBUG = True
