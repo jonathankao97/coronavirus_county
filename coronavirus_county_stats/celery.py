ee654a01fa96738acd6ce011398a52d1a1b2057f
Jonathan Kao <jkao97@yahoo.com>
2020-03-19 15:33:33 -0700

    update schedule

--- coronavirus_county_stats/celery.py
+++ coronavirus_county_stats/celery.py
@@ -2,6 +2,8 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coronavirus_county_stats.settings')
 
 app = Celery('coronavirus_county_stats')
@@ -18,7 +20,8 @@ app.conf.beat_schedule = {
 
     'sync_data': {
         'task': 'sync_data',
-        'schedule': 10800.0,
+        # 'schedule': 10800.0,
+        'schedule': crontab(minute=0, hour='*/3'),
         'args': ()
     },
 
