a645f8be10dbf01507cc9b81ae7c92424a05f221
Jonathan Kao <jkao97@yahoo.com>
2020-03-19 15:12:48 -0700

    remove celery results

--- coronavirus_county_stats/settings.py
+++ coronavirus_county_stats/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_celery_results',
+    # 'django_celery_results',
 
     'county',
 ]
