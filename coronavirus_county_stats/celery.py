4c620cd685bee92c21248096bae0188317526968
Jonathan Kao <jkao97@yahoo.com>
2020-03-19 19:25:32 -0700

    move schedule

--- coronavirus_county_stats/celery.py
+++ coronavirus_county_stats/celery.py
@@ -2,7 +2,6 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coronavirus_county_stats.settings')
 
@@ -16,13 +15,3 @@ def debug_task(self):
     print('Request: {0!r}'.format(self.request))
 
 
-app.conf.beat_schedule = {
-
-    'sync_data': {
-        'task': 'sync_data',
-        # 'schedule': 10800.0,
-        'schedule': crontab(minute=0, hour='*/3'),
-        'args': ()
-    },
-
-}
