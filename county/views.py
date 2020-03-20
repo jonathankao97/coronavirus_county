a47f574947d28c262eef31498a17262cd3d2d632
cozie <vvictor.llin@gmail.com>
2020-03-20 14:46:04 -0700

    added chart time interval switching

--- county/views.py
+++ county/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from county.models import City, County
+from county.models import City, County, State
 from django.utils import timezone
 from datetime import timedelta
 
@@ -63,30 +63,49 @@ us_state_abbrev = {
 
 
 def test(request):
-
     return render(request, 'test.html')
 
 
-def data(request, county_id):
-    county = County.objects.get(id=county_id)
-    confirmed = county.get_confirmed()
-    confirmed_delta = confirmed[-1] - confirmed[-2]
-    deaths = county.get_deaths()
-    deaths_delta = deaths[-1] - deaths[-2]
+def hello(request):
+    return render(request, 'hello.html')
 
+
+def data(request, county_id):
+    sups = ["aux", "st", "nd", "rd", "th"]
+    county = County.objects.get(id=county_id)
+    confirmed = day_array_converter(county.get_confirmed(), 30)
+    confirmed_delta = 0
+    if len(confirmed) > 1:
+        confirmed_delta = confirmed[-1] - confirmed[-2]
+    confirmed_increase = 0
+    if len(confirmed) >= 2 and confirmed[-2] != 0:
+        confirmed_increase = int(float(confirmed_delta * 100) / confirmed[-2])
+    deaths = day_array_converter(county.get_deaths(), 30)
+    deaths_delta = 0
+    if len(deaths) > 1:
+        deaths_delta = deaths[-1] - deaths[-2]
+    deaths_increase = 0
+    if len(deaths) >= 2 and deaths[-2] != 0:
+        deaths_increase = int(float(deaths_increase * 100) / deaths[-2])
+    county_rank = int(county.get_state_county_ranking()[-1])
+    state_rank = int(county.state.get_state_ranking()[-1])
     context = {
         'confirmed': confirmed,
         'confirmed_delta': confirmed_delta,
-        'confirmed_increase': int(float(confirmed_delta * 100) / confirmed[-2]) if confirmed[-2] != 0 else 0,
+        'confirmed_increase': confirmed_increase,
         'deaths': deaths,
         'deaths_delta': deaths_delta,
-        'death_increase': int(float(confirmed_delta * 100) / confirmed[-2]) if deaths[-2] != 0 else 0,
-        'county_rank': county.state_county_ranking,
-        'x_axis': [],
+        'death_increase': deaths_increase,
+        'county_rank': county_rank,
+        'county_rank_sup': sups[min(4, county_rank)],
+        'state_rank': state_rank,
+        'state_rank_sup': sups[min(4, state_rank)],
+        'county': county,
+        'state': county.state,
+        'x_axis': []
     }
-
     now = timezone.localtime(timezone.now())
-    for i in range(0, min(len(confirmed), 14)):
+    for i in range(0, min(len(confirmed), 30)):
         context['x_axis'].append(now.strftime('%-m/%-d'))
         now -= timedelta(1)
     context['x_axis'].reverse()
@@ -133,3 +152,18 @@ def add_county(queries, county, note):
         county.note = note
         county.state_initials = us_state_abbrev[county.state.name]
         queries.add(county)
+
+
+def day_array_converter(arr, days):
+    length = len(arr)
+    ret = []
+    sum = 0
+    for i in range(0, length):
+        if i/8 == days:
+            break
+        index = length - 1 - i
+        sum += arr[index]
+        if i % 8 == 7:
+            ret.append(int(sum / 8.0))
+    return ret
+
--- static/js/chart-area-demo.js
+++ static/js/chart-area-demo.js
@@ -25,16 +25,24 @@ function number_format(number, decimals, dec_point, thousands_sep) {
   }
   return s.join(dec);
 }
-
+var myLineChart
+var myOtherLineChart
 // Area Chart Example
-var ctx = document.getElementById("myAreaChart");
-var ctx1 = document.getElementById("myDeathChart");
-var myLineChart = new Chart(ctx, {
+function make_charts(slice){
+beg = x_axis.length - Math.min(slice, x_axis.length)
+if (myLineChart){
+    console.log("hi")
+    myLineChart.destroy();
+    myOtherLineChart.destroy();
+    }
+var canvas = document.getElementById("myAreaChart");
+var canvas1 = document.getElementById("myDeathChart");
+myLineChart = new Chart(canvas, {
   type: 'line',
   data: {
-    labels: x_axis,
+    labels: x_axis.slice(beg),
     datasets: [{
-      label: "Cases",
+      label: "Confirmed",
       lineTension: 0,
       backgroundColor: "rgba(78, 115, 223, 0.05)",
       borderColor: "rgba(255, 255, 255, 0.8)",
@@ -46,91 +54,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(255, 255, 255, 0)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: confirmed,
-    }],
-  },
-  options: {
-    maintainAspectRatio: false,
-    layout: {
-      padding: {
-        left: 10,
-        right: 25,
-        top: 5,
-        bottom: 0
-      }
-    },
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'date'
-        },
-        gridLines: {
-          display: false,
-          drawBorder: false
-        },
-        ticks: {
-          maxTicksLimit: 7,
-          fontSize: 13,
-          fontFamily: "Helvetica-light"
-        }
-      }],
-      yAxes: [{
-        ticks: {
-          maxTicksLimit: 5,
-          padding: 20,
-          min: 380,
-          // Include a dollar sign in the ticks
-        },
-        gridLines: {
-          color: "rgba(255, 255, 255, 0.4)",
-          zeroLineColor: "rgba(255, 255, 255, 0.4)",
-          drawBorder: false,
-          borderDash: [2],
-          zeroLineBorderDash: [2]
-        }
-      }],
-    },
-    legend: {
-      display: false
-    },
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      titleMarginBottom: 10,
-      titleFontColor: '#6e707e',
-      titleFontSize: 14,
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      intersect: false,
-      mode: 'index',
-      caretPadding: 10,
-      callbacks: {
-      }
-    }
-  }
-});
-
-var myOtherLineChart = new Chart(ctx1, {
- type: 'line',
-  data: {
-    labels: x_axis,
-    datasets: [{
-      label: "Deaths",
-      lineTension: 0,
-      backgroundColor: "rgba(78, 115, 223, 0.05)",
-      borderColor: "rgba(255, 255, 255, 0.8)",
-      pointRadius: 5,
-      pointBackgroundColor: "rgba(255, 255, 255, .8)",
-      pointBorderColor: "rgba(255, 255, 255, 0)",
-      pointHoverRadius: 4,
-      pointHoverBackgroundColor: "rgba(255, 255, 255, .8)",
-      pointHoverBorderColor: "rgba(255, 255, 255, 0)",
-      pointHitRadius: 10,
-      pointBorderWidth: 2,
-      data: deaths,
+      data: confirmed.slice(beg),
     }],
   },
   options: {
@@ -196,3 +120,89 @@ var myOtherLineChart = new Chart(ctx1, {
     }
   }
 });
+
+myOtherLineChart = new Chart(canvas1, {
+ type: 'line',
+  data: {
+    labels: x_axis.slice(beg),
+    datasets: [{
+      label: "Deaths",
+      lineTension: 0,
+      backgroundColor: "rgba(78, 115, 223, 0.05)",
+      borderColor: "rgba(255, 255, 255, 0.8)",
+      pointRadius: 5,
+      pointBackgroundColor: "rgba(255, 255, 255, .8)",
+      pointBorderColor: "rgba(255, 255, 255, 0)",
+      pointHoverRadius: 4,
+      pointHoverBackgroundColor: "rgba(255, 255, 255, .8)",
+      pointHoverBorderColor: "rgba(255, 255, 255, 0)",
+      pointHitRadius: 10,
+      pointBorderWidth: 2,
+      data: deaths.slice(beg),
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    layout: {
+      padding: {
+        left: 10,
+        right: 25,
+        top: 5,
+        bottom: 0
+      }
+    },
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false
+        },
+        ticks: {
+          maxTicksLimit: 7,
+          fontSize: 13,
+          fontFamily: "Helvetica-light"
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          maxTicksLimit: 5,
+          padding: 20,
+          min: 0,
+          // Include a dollar sign in the ticks
+        },
+        gridLines: {
+          color: "rgba(255, 255, 255, 0.4)",
+          zeroLineColor: "rgba(255, 255, 255, 0.4)",
+          drawBorder: false,
+          borderDash: [2],
+          zeroLineBorderDash: [2]
+        }
+      }],
+    },
+    legend: {
+      display: false
+    },
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      titleMarginBottom: 10,
+      titleFontColor: '#6e707e',
+      titleFontSize: 14,
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      intersect: false,
+      mode: 'index',
+      caretPadding: 10,
+      callbacks: {
+      }
+    }
+  }
+});
+}
+make_charts(7)
