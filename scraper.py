9cbcda588a1390f74f891dfdde5a96d42659681f
Jonathan Kao <jkao97@yahoo.com>
2020-03-22 13:22:57 -0700

    update scraper

--- scraper.py
+++ scraper.py
@@ -40,18 +40,18 @@ def parse_state(counter, state_info):
 
 
 def sync_data():
-    # chrome_options = webdriver.ChromeOptions()
-    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    # chrome_options.add_argument("--headless")
-    # chrome_options.add_argument("--disable-dev-shm-usage")
-    # chrome_options.add_argument("--no-sandbox")
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
     # #
-    # browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    # browser.get('https://coronavirus.1point3acres.com/en')
-
-    browser = webdriver.Chrome('/Users/jkao97/downloads/chromedriver')
-    # browser = webdriver.Chrome('/Users/JonKao/downloads/chromedriver')
+    browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     browser.get('https://coronavirus.1point3acres.com/en')
+
+    # browser = webdriver.Chrome('/Users/jkao97/downloads/chromedriver')
+    # browser = webdriver.Chrome('/Users/JonKao/downloads/chromedriver')
+    # browser.get('https://coronavirus.1point3acres.com/en')
 
     # print(browser.page_source)
 
